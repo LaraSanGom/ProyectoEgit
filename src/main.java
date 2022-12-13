9ebd4164f68eaa12325b76204de70fd2cf78578f
Lara <lsangom1111@g.educaand.es>
2022-12-13 22:45:32 +0100

    Principal2.2

--- src/main.java
+++ src/main.java
@@ -11,4 +11,8 @@ import java.util.*;
 		public static int producto(int a, int b) {
 			int c; c = a * b;
 			return c; }
+		public static int dividir(int a, int b) {
+			int c; c = a / b;
+			return c; }
+		
 	} }
