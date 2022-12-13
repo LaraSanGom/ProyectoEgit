1ad9490fb34cb3be72c104b6393cec466c6bc7a8
Lara <lsangom1111@g.educaand.es>
2022-12-13 22:38:00 +0100

    Principal1.2

--- src/main.java
+++ src/main.java
@@ -13,4 +13,7 @@ import java.util.*;
 			return c; }
 		resultado = suma(numero1, numero2);
 		System.out.println("Suma: " + resultado);
+		public static int resta(int a, int b) {
+			int c; c = a - b;
+			return c; }
 	} }
